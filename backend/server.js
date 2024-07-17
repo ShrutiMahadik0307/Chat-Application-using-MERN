19e67e83112fb5c201a01522a2a062e7c14e6b24
Your Name <shruti.mahadik0307@gmail.com>
2024-07-18 00:41:22 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; 
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); //parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+//app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+    //es.send("Hello World");
+//});
+
+
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
